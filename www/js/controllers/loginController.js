4c7cf8413f2d74300db8494906ba224a16db88e5
2016ht13470 <2016ht13470@wilp.bits-pilani.ac.in>
2018-03-14 00:37:18 +0530

    login

--- www/js/controllers/loginController.js
+++ www/js/controllers/loginController.js
@@ -1,7 +1,17 @@
 OfflineSyncApp.controller('loginController', ['$scope', '$state', function($scope, $state)
 {
+    $scope.username = "" ;
+    $scope.password = "";
     $scope.goToList = function()
     {
-        $state.go('list');
+        //$state.go('list');
+        if($scope.username == "tempUser" && $scope.password == "test")
+        {
+            $state.go('list');
+        }
+        else
+        {
+            $scope.error = true;
+        }
     }
 }]);
